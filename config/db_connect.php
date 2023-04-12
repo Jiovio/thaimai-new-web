bf2678ae7569d8c5e8f7cf1dfe1368d525e13371
Siva <sivaramandroiddev@outlook.com>
2023-04-12 19:18:22 +0530

    changed con

--- config/db_connect.php
+++ config/db_connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $dbuser = "adminthaimai_user";
 $dbpwd = "ELxKb+Tp2GHn";
-$dbname = "adminthaimai_admin";
+$dbname = "thaimaicloudonwe_dev";
 $conn = mysqli_connect($host, $dbuser, $dbpwd, $dbname) or die("Thaimayudan Connection failed: " . mysqli_connect_error());
 if ($conn->connect_error) { 
     echo "Thaimayudan Connection failed"; 
