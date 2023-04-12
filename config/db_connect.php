c24c0235fabb8fba0d48ee781d87100720190994
Siva <sivaramandroiddev@outlook.com>
2023-04-12 16:47:04 +0530

    changes in hfilter file

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,13 +1,13 @@
 <?php
 $host = "localhost";
-$dbuser = "adminthaimai_user";
-$dbpwd = "ELxKb+Tp2GHn";
-$dbname = "adminthaimai_admin";
+$dbuser = "root";
+$dbpwd = "";
+$dbname = "thaim";
 
 $conn = mysqli_connect($host, $dbuser, $dbpwd, $dbname) or die("Thaimayudan Connection failed: " . mysqli_connect_error());
 if ($conn->connect_error) { 
     echo "Thaimayudan Connection failed"; 
 }
-$siteurl = "http://admin.thaimaiyudan.org";
-
+//$siteurl = "http://admin.thaimaiyudan.org";
+$siteurl = "https://thaimai.cloudonweb.in";
 ?>
