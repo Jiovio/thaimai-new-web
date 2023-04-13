4f248f23431d61f815279c000ff991f5e9d65f38
Siva <sivaramandroiddev@outlook.com>
2023-04-13 11:56:36 +0530

    set gravida value in medical history

---
+++ forms/getGravida.php
@@ -0,0 +1,11 @@
+<?php
+include "../config/db_connect.php";
+$picmeNo = $_POST["picmeno"];
+$result = mysqli_query($conn,"SELECT gravida FROM anregistration WHERE picmeno = '$picmeNo' order by id desc LIMIT 0,1;");
+
+$result1 ="";
+while($res = mysqli_fetch_array($result)){  
+    $result1 = $res['gravida'];
+}
+echo $result1;
+?>
