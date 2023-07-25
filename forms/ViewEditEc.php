8059d9ac28039982a5147fa5d023623c492ef646
nithyajiovio <nithya.jiovio@outlook.com>
2023-07-25 16:44:45 +0530

    Test Plan bugs fixed for Plan 1

--- forms/ViewEditEc.php
+++ forms/ViewEditEc.php
@@ -88,7 +88,7 @@ if (! empty($_POST["update"])) {
                 <div class="col-xl">
                   <div class="card mb-4">
                     <div class="card-header d-flex justify-content-between align-items-center">
-						<h4 class="fw-bold"><span class="text-muted fw-light">Mother Details</span></h4>
+						<h4 class="fw-bold"><span class="text-muted fw-light">Mother's Details</span></h4>
                         <small class="text-muted float-end"><span class="mand">* </span> Fields are Mandatory</small>
                     </div>
                     <div class="card-body">
@@ -165,7 +165,7 @@ if (! empty($_POST["update"])) {
                         </div>
 
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-phone">MOTHER NAME AS PER AADHAAR <span class="mand">* </span><span id="errMName"></span></label>
+                          <label class="form-label" for="basic-icon-default-phone">MOTHER'S NAME AS PER AADHAAR <span class="mand">* </span><span id="errMName"></span></label>
                           <div class="input-group input-group-merge">
                             <span id="basic-icon-default-mobile" class="input-group-text"
                               ><i class="bx bx-user-circle"></i></span>
@@ -174,8 +174,8 @@ if (! empty($_POST["update"])) {
                               name="motheraadhaarname"
                               id="motheraadhaarname" 
                               class="form-control phone-mask"
-                              placeholder="MOTHER NAME AS PER AADHAAR"
-                              aria-label="MOTHER NAME AS PER AADHAAR"
+                              placeholder="MOTHER'S NAME AS PER AADHAAR"
+                              aria-label="MOTHER'S NAME AS PER AADHAAR"
                               aria-describedby="basic-icon-default-mobile"
                               disabled value="<?php echo $maadhaarname; ?>"
                             />
@@ -185,7 +185,7 @@ if (! empty($_POST["update"])) {
 
                         <div class="row">
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-phone">MOTHER FULLNAME <span class="mand">* </span><span id="errMfullname"></span></label>
+                          <label class="form-label" for="basic-icon-default-phone">MOTHER'S FULL NAME <span class="mand">* </span><span id="errMfullname"></span></label>
                           <div class="input-group input-group-merge">
                             <span id="basic-icon-default-mobile" class="input-group-text"
                               ><i class="bx bx-user-check"></i
@@ -195,8 +195,8 @@ if (! empty($_POST["update"])) {
                               name="motherfullname"
                               id="motherfullname"
                               class="form-control phone-mask"
-                              placeholder="MOTHER FULLNAME"
-                              aria-label="MOTHER FULLNAME"
+                              placeholder="MOTHER'S FULL NAME"
+                              aria-label="MOTHER'S FULL NAME"
                               aria-describedby="basic-icon-default-mobile"
                               disabled value="<?php echo $mfullname; ?>"
                               
@@ -205,7 +205,7 @@ if (! empty($_POST["update"])) {
                         </div>
 
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-email">MOTHER DATE OF BIRTH <span class="mand">* </span><span id="errMdob"></span></label>
+                          <label class="form-label" for="basic-icon-default-email">MOTHER'S DATE OF BIRTH <span class="mand">* </span><span id="errMdob"></span></label>
                           <div class="input-group input-group-merge">
                             <span class="input-group-text"><i class="bx bx-calendar"></i></span>
                             <input
@@ -224,7 +224,7 @@ if (! empty($_POST["update"])) {
                         </div>
                         <div class="row">
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-phone">MOTHER AGE AT MARRIAGE <span class="mand">* </span><span id="errMoAgeMrg"></span></label>
+                          <label class="form-label" for="basic-icon-default-phone">MOTHER'S AGE AT MARRIAGE <span class="mand">* </span><span id="errMoAgeMrg"></span></label>
                           <div class="input-group input-group-merge">
                             <span id="basic-icon-default-mobile" class="input-group-text"
                               ><i class="bx bx-user-pin"></i
@@ -235,8 +235,8 @@ if (! empty($_POST["update"])) {
                               name="motheragemarriage"
                               id="motheragemarriage"
                               class="form-control phone-mask"
-                              placeholder="MOTHER AGE AT MARRIAGE"
-                              aria-label="MOTHER AGE AT MARRIAGE"
+                              placeholder="MOTHER'S AGE AT MARRIAGE"
+                              aria-label="MOTHER'S AGE AT MARRIAGE"
                               aria-describedby="basic-icon-default-mobile"
                               disabled value="<?php echo $magemarriage; ?>"
                               
@@ -244,7 +244,7 @@ if (! empty($_POST["update"])) {
                           </div>
                         </div>
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-phone">MOTHER AGE AT EC Registration <span class="mand">* </span><span id="errMageecreg"></span></label>
+                          <label class="form-label" for="basic-icon-default-phone">MOTHER'S AGE AT EC REGISTRATION <span class="mand">* </span><span id="errMageecreg"></span></label>
                           <div class="input-group input-group-merge">
                             <span id="basic-icon-default-mobile" class="input-group-text"
                               ><i class="bx bx-user-minus"></i
@@ -256,8 +256,8 @@ if (! empty($_POST["update"])) {
                               name="motherageecreg"
                               id="motherageecreg" 
                               class="form-control phone-mask"
-                              placeholder="MOTHER AGE AT REGISTRATION"
-                              aria-label="MOTHER AGE AT REGISTRATION"
+                              placeholder="MOTHER'S AGE AT EC REGISTRATION"
+                              aria-label="MOTHER'S AGE AT EC REGISTRATION"
                               aria-describedby="basic-icon-default-mobile"
                               disabled value="<?php echo $mageecreg; ?>"
                             />
@@ -319,7 +319,7 @@ if (! empty($_POST["update"])) {
 
                       
                       <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-phone">MOTHER EDUCATIONAL STATUS <span class="mand">* </span><span id="errMedustatus"></span></label>
+                          <label class="form-label" for="basic-icon-default-phone">MOTHER'S EDUCATIONAL STATUS <span class="mand">* </span><span id="errMedustatus"></span></label>
                           <div class="input-group input-group-merge">
                           <select name="motheredustatus" id="motheredustatus" class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon" value="<?php echo $motheredustatus; ?>" disabled>
                             
@@ -358,7 +358,7 @@ if (! empty($_POST["update"])) {
                 <div class="col-xl">
                   <div class="card mb-4">
                     <div class="card-header d-flex justify-content-between align-items-center">
-						<h4 class="fw-bold"><span class="text-muted fw-light">Father Details</span></h4>
+						<h4 class="fw-bold"><span class="text-muted fw-light">Father's Details</span></h4>
                         <small class="text-muted float-end"><span class="mand">* </span> Fields are Mandatory</small>
                     </div>
                     <div class="card-body">
@@ -366,7 +366,7 @@ if (! empty($_POST["update"])) {
                		<input type="hidden" name="id" disabled value="<?php echo $id; ?>">
 						<div class="row">
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-password">HUSBAND AADHAAR ID <span id="errHaadhaarid"></span></label>
+                          <label class="form-label" for="basic-icon-default-password">HUSBAND'S AADHAAR ID <span id="errHaadhaarid"></span></label>
                           <div class="input-group input-group-merge">
                             <label class="lblViolet"><?php echo $haadhaarid; ?>
                               </label>
@@ -377,8 +377,8 @@ if (! empty($_POST["update"])) {
                               id="husbandaadhaarid"
                               maxlength="12"
                               class="form-control"
-                              placeholder="HUSBAND AADHAAR ID"
-                              aria-label="HUSBAND AADHAAR ID"
+                              placeholder="HUSBAND'S AADHAAR ID"
+                              aria-label="HUSBAND'S AADHAAR ID"
                               aria-describedby="basic-icon-default-password2"
                              value="<?php echo $haadhaarid; ?>"
                             />
@@ -386,7 +386,7 @@ if (! empty($_POST["update"])) {
                         </div>
 
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-phone">HUSBAND NAME AS PER AADHAAR <span class="mand">* </span><span id="errhaadhaarname"></span></label>
+                          <label class="form-label" for="basic-icon-default-phone">HUSBAND'S NAME AS PER AADHAAR <span class="mand">* </span><span id="errhaadhaarname"></span></label>
                           <div class="input-group input-group-merge">
                             <span id="basic-icon-default-mobile" class="input-group-text"
                               ><i class="bx bx-user"></i
@@ -396,8 +396,8 @@ if (! empty($_POST["update"])) {
                               name="husbandaadhaarname"
                               id="husbandaadhaarname" 
                               class="form-control phone-mask"
-                              placeholder="HUSBAND NAME AS PER AADHAAR"
-                              aria-label="HUSBAND NAME AS PER AADHAAR"
+                              placeholder="HUSBAND'S NAME AS PER AADHAAR"
+                              aria-label="HUSBAND'S NAME AS PER AADHAAR"
                               aria-describedby="basic-icon-default-mobile"
                               disabled value="<?php echo $haadhaarname; ?>"
                             />
@@ -405,7 +405,7 @@ if (! empty($_POST["update"])) {
                         </div>
 
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-phone">HUSBAND FULLNAME <span class="mand">* </span><span id="errhfullname"></span></label>
+                          <label class="form-label" for="basic-icon-default-phone">HUSBAND'S FULL NAME <span class="mand">* </span><span id="errhfullname"></span></label>
                           <div class="input-group input-group-merge">
                             <span id="basic-icon-default-mobile" class="input-group-text"
                               ><i class="bx bx-user-check"></i
@@ -415,8 +415,8 @@ if (! empty($_POST["update"])) {
                               name="husfullname"
                               id="husfullname"
                               class="form-control phone-mask"
-                              placeholder="HUSBAND FULLNAME"
-                              aria-label="HUSBAND FULLNAME"
+                              placeholder="HUSBAND'S FULL NAME"
+                              aria-label="HUSBAND'S FULL NAME"
                               aria-describedby="basic-icon-default-mobile"
                               disabled value="<?php echo $hfullname; ?>"
                               
@@ -425,7 +425,7 @@ if (! empty($_POST["update"])) {
                         </div>
 
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-email">HUSBAND DATE OF BIRTH <span class="mand">* </span><span id="errhdob"></span></label>
+                          <label class="form-label" for="basic-icon-default-email">HUSBAND'S DATE OF BIRTH <span class="mand">* </span><span id="errhdob"></span></label>
                           <div class="input-group input-group-merge">
                             <span class="input-group-text"><i class="bx bx-calendar"></i></span>
                             <input
@@ -443,7 +443,7 @@ if (! empty($_POST["update"])) {
                         </div>
                         
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-phone">HUSBAND AGE AT MARRIAGE <span class="mand">* </span><span id="errhagemarriage"></span></label>
+                          <label class="form-label" for="basic-icon-default-phone">HUSBAND'S AGE AT MARRIAGE <span class="mand">* </span><span id="errhagemarriage"></span></label>
                           <div class="input-group input-group-merge">
                             <span id="basic-icon-default-mobile" class="input-group-text"
                               ><i class="bx bx-user-pin"></i
@@ -454,8 +454,8 @@ if (! empty($_POST["update"])) {
                               name="husagemarriage"
                               id="husagemarriage"
                               class="form-control phone-mask"
-                              placeholder="HUSBAND AGE AT CONCEPTION"
-                              aria-label="HUSBAND AGE AT CONCEPTION"
+                              placeholder="HUSBAND'S AGE AT CONCEPTION"
+                              aria-label="HUSBAND'S AGE AT CONCEPTION"
                               aria-describedby="basic-icon-default-mobile"
                               disabled value="<?php echo $hagemarriage; ?>"
                               
@@ -465,7 +465,7 @@ if (! empty($_POST["update"])) {
                         </div>
 
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-phone">HUSBAND AGE AT EC Registration <span class="mand">* </span><span id="errhageecreg"></span></label>
+                          <label class="form-label" for="basic-icon-default-phone">HUSBAND'S AGE AT EC Registration <span class="mand">* </span><span id="errhageecreg"></span></label>
                           <div class="input-group input-group-merge">
                             <span id="basic-icon-default-mobile" class="input-group-text"
                               ><i class="bx bx-user-minus"></i
@@ -476,8 +476,8 @@ if (! empty($_POST["update"])) {
                               name="husageecreg"
                               id="husageecreg"
                               class="form-control phone-mask"
-                              placeholder="HUSBAND AGE AT MARRIAGE"
-                              aria-label="HUSBAND AGE AT MARRIAGE"
+                              placeholder="HUSBAND'S AGE AT MARRIAGE"
+                              aria-label="HUSBAND'S AGE AT MARRIAGE"
                               aria-describedby="basic-icon-default-mobile"
                               disabled value="<?php echo $hageecreg; ?>"
                               
@@ -486,7 +486,7 @@ if (! empty($_POST["update"])) {
                         </div>
 
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-phone">HUSBAND MOBILE NUMBER <span class="mand">* </span><span id="errhmob"></span></label>
+                          <label class="form-label" for="basic-icon-default-phone">HUSBAND'S MOBILE NUMBER <span class="mand">* </span><span id="errhmob"></span></label>
                           <div class="input-group input-group-merge">
                             <span id="basic-icon-default-mobile" class="input-group-text"
                               ><i class="bx bx-mobile"></i
@@ -498,8 +498,8 @@ if (! empty($_POST["update"])) {
                               id="husmobno"
                               maxlength="10"
                               class="form-control phone-mask"
-                              placeholder="HUSBAND MOBILE NUMBER"
-                              aria-label="HUSBAND MOBILE NUMBER"
+                              placeholder="HUSBAND'S MOBILE NUMBER"
+                              aria-label="HUSBAND'S MOBILE NUMBER"
                               aria-describedby="basic-icon-default-mobile"
                               disabled value="<?php echo $husmobno; ?>"
                               
@@ -508,7 +508,7 @@ if (! empty($_POST["update"])) {
                         </div>
   
                         <div class="col-6 mb-3">
-                          <label class="form-label" for="basic-icon-default-phone">HUSBAND EDUCATIONAL STATUS <span class="mand">* </span><span id="errhedustatus"></span></label>
+                          <label class="form-label" for="basic-icon-default-phone">HUSBAND'S EDUCATIONAL STATUS <span class="mand">* </span><span id="errhedustatus"></span></label>
                           <div class="input-group input-group-merge">
                           <select name="husedustatus" id="husedustatus" class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon" value="<?php echo $husedustatus; ?>" disabled> 
                                 <?php
