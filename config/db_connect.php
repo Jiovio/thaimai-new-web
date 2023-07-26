26d4a34196ec16bc11acca417369a2f903d17926
nithyajiovio <nithya.jiovio@outlook.com>
2023-07-26 18:22:34 +0530

    db_connect siteurl changed

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$dbuser = "root";
-$dbpwd = "";
-$dbname = "thaimai_cloud";
+$dbuser = "thaimaicloudonwe_dev-user";
+$dbpwd = "=t1VE89[*7w_";
+$dbname = "thaimaicloudonwe_dev";
 $conn = mysqli_connect($host, $dbuser, $dbpwd, $dbname) or die("Thaimayudan Connection failed: " . mysqli_connect_error());
 if ($conn->connect_error) { 
     echo "Thaimayudan Connection failed"; 
