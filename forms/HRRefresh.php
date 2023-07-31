320eab25d4db96b45f0075266caf9d3d9733bd8b
nithyajiovio <nithya.jiovio@outlook.com>
2023-07-31 11:09:01 +0530

    Checking Pull for New Branch

--- forms/HRRefresh.php
+++ forms/HRRefresh.php
@@ -113,7 +113,7 @@ anregistration.childDeath > 2)");
 	/* ------------------------------------------------------- Refresh Message -----------------------------------------------------------*/
      
 	 
-	 echo "<script>alert('Refreshed Successfully!!!');</script>;window.location.replace('{$siteurl}/forms/highRiskMothers.php');</script>";
+	 echo "<script>alert('Refreshed Successfully!!!');window.location.replace('{$siteurl}/forms/highRiskMothers.php');</script>";
          header("Location:".$redirectUrl);
 	//exit; 
 
