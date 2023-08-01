0523f6ff6ed44928ad8d5b1ba6a1b2dac22d45d3
nithyajiovio <nithya.jiovio@outlook.com>
2023-08-01 14:37:15 +0530

    forms/Immunization.php

--- forms/Immunization.php
+++ forms/Immunization.php
@@ -45,7 +45,7 @@
   $listQry = "SELECT DISTINCT(im.picmeNo),im.id,im.doseNo,im.doseDueDate,im.doseProvidedDate,im.breastFeeding,ec.motheraadhaarname,im.createdUserId,ec.BlockId,ec.PhcId,ec.HscId FROM immunization im JOIN ecregister ec on ec.picmeNo=im.picmeNo WHERE im.status=1";
   $private = " AND im.createdUserId='".$userid."'";
  // $orderQry = " ORDER BY ec.motheraadhaarname ASC";
- $orderQry = " ORDER BY im.doseNo ASC";
+ $orderQry = " ORDER BY im.picmeNo + im.doseNo ASC";
 
     if(($usertype == 0) || ($usertype == 1)) {
             if(isset($_POST['filter'])) {
