7da0e9aa87db8caeac3119fff479c695150994e1
nithyajiovio <nithya.jiovio@outlook.com>
2023-08-10 20:03:36 +0530

    putting code for refresh

--- forms/HRRefresh.php
+++ forms/HRRefresh.php
@@ -6,7 +6,7 @@
   
    
     /* ------------------------------------------------------- AN Registration --------------------------------------------------------------------*/
-    $listQry_anreg_ins = mysqli_query($conn, "INSERT INTO highriskmothers (picmeNo) SELECT DISTINCT(picmeno),status from anregistration WHERE (anregistration.gravida > 2 OR anregistration.livingChildren > 2 OR anregistration.abortion > 2 OR anregistration.childDeath > 2 OR anregistration.para > 2 OR anregistration.motherWeight <= 40 OR anregistration.bpSys >= 140 OR anregistration.bpDia >= 90 OR anregistration.MotherAge < 18) 
+    $listQry_anreg_ins = mysqli_query($conn, "INSERT INTO highriskmothers (picmeNo) SELECT DISTINCT(picmeno) from anregistration WHERE (anregistration.gravida > 2 OR anregistration.livingChildren > 2 OR anregistration.abortion > 2 OR anregistration.childDeath > 2 OR anregistration.para > 2 OR anregistration.motherWeight <= 40 OR anregistration.bpSys >= 140 OR anregistration.bpDia >= 90 OR anregistration.MotherAge < 18) 
 AND NOT EXISTS (SELECT antenatalvisit.picmeno FROM antenatalvisit WHERE antenatalvisit.picmeno = anregistration.picmeno)
 AND NOT EXISTS (SELECT deliverydetails.picmeno FROM deliverydetails WHERE deliverydetails.picmeno = anregistration.picmeno)");  
 
