76445b9f87ed81c7901b5ce17b58c50c098db7c2
nithyajiovio <nithya.jiovio@outlook.com>
2023-09-11 12:53:28 +0530

    Conception Age Validation Given

--- forms/ViewEditAntenatal.php
+++ forms/ViewEditAntenatal.php
@@ -534,7 +534,6 @@ if (isset($_GET['del'])) {
 	                    $rec_Mage = "";
 	                    $rec_Mage = $n_rec['motheragemarriage'];
 						?>
-						
                         <div class="mb-3 col-md-6">
                             <label class="form-label">Mother's Age at Conception <span class="mand">* </span></label>
                             <div class="input-group input-group-merge">
@@ -551,20 +550,12 @@ if (isset($_GET['del'])) {
                             />
                           </div>
                           </div>
-						  
-                          
-						  <?php 
-						$rec_pic = mysqli_query($conn, "SELECT * FROM ecregister WHERE picmeno = $picmeno");
-			            $n_rec = mysqli_fetch_array($rec_pic);
-	                    $rec_Hage = "";
-	                    $rec_Hage = $n_rec['husagemarriage'];
-						?>
                           <div class="mb-3 col-md-6">
                             <label class="form-label">Husband's Age at Conception <span class="mand">* </span></label>
                             <div class="input-group input-group-merge">
                             <input
                               type="number"
-                              min="<?php echo $rec_Hage; ?>" max="99"
+                              min="11" max="99"
                               class="form-control"
                               id="HusbandAgeChk"
                               name="HusbandAge"
