90185682c5c44cd38f9313fa311c7142c4a2ce94
nithyajiovio <nithya.jiovio@outlook.com>
2024-01-24 23:25:35 +0530

    Fixed pregnancy status bugs

--- forms/LoadBlock.php
+++ forms/LoadBlock.php
@@ -27,7 +27,8 @@ $PvCnt = mysqli_fetch_array($PvCntmq);
 $UsCntmq = mysqli_query($conn,"SELECT COUNT(id) AS UsCnt FROM users WHERE BlockId='".$bloName."' AND status=1");
 $UsCnt = mysqli_fetch_array($UsCntmq);
     
-$LmCntmq = mysqli_query($conn,"SELECT COUNT(id) AS LmCnt FROM ecregister WHERE status NOT IN(0,1) AND BlockId='".$bloName."'");
+$LmCntmq = mysqli_query($conn,"SELECT COUNT(id) AS LmCnt FROM ecregister WHERE status NOT IN(0,1)  
+AND NOT EXISTS (SELECT deliverydetails.picmeno FROM deliverydetails WHERE deliverydetails.picmeno = ecregister.picmeNo) AND BlockId='".$bloName."'");
 $LmCnt = mysqli_fetch_array($LmCntmq);
     
 $HsCntmq = mysqli_query($conn,"SELECT COUNT(id) AS HsCnt FROM hscmaster WHERE BlockId='".$bloName."'");
