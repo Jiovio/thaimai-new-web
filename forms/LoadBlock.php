acb192aa93a4849118f3aff708dd4cdd06d91961
nithyajiovio <nithya.jiovio@outlook.com>
2024-02-04 01:29:56 +0530

    HR_virtual table creation

--- forms/LoadBlock.php
+++ forms/LoadBlock.php
@@ -30,7 +30,9 @@ $PvCnt = mysqli_fetch_array($PvCntmq);
 $UsCntmq = mysqli_query($conn,"SELECT COUNT(id) AS UsCnt FROM users WHERE BlockId='".$bloName."' AND status=1");
 $UsCnt = mysqli_fetch_array($UsCntmq);
    
-$LmCntmq = mysqli_query($conn,"SELECT COUNT(id) AS LmCnt FROM ecregister WHERE status NOT IN(0,1)
+$LmCntmq = mysqli_query($conn,"SELECT COUNT(id) AS LmCnt FROM ecregister 
+JOIN hscmaster hs on ecregister.BlockId = hs.BlockId
+WHERE status NOT IN(0,1)
 AND NOT EXISTS (SELECT deliverydetails.picmeno FROM deliverydetails WHERE deliverydetails.picmeno = ecregister.picmeNo)
 AND BlockId='".$bloName."'");
 $LmCnt = mysqli_fetch_array($LmCntmq);
