d3ed11fc8d3ea83808f9cd0ddb7c8414d3ad9d72
nithyajiovio <nithya.jiovio@outlook.com>
2024-02-10 18:33:14 +0530

    Fixed filteration issues

--- forms/ajax/getPanchayat.php
+++ forms/ajax/getPanchayat.php
@@ -3,14 +3,27 @@ include "../../config/db_connect.php";
 if (isset($_POST['type']) && !empty($_POST['type'])) {
     if ($_POST['type'] == 'hsc') {
         $hscId = $_POST["hscId"];
-        $result = mysqli_query($conn, "SELECT DISTINCT PanchayatId, PanchayatName, PhcId FROM hscmaster WHERE HscId = '$hscId' ORDER BY PanchayatId;");
+		$BlockId = $_POST["BlockId"];
+		$PhcId = $_POST["PhcId"];
+        $result = mysqli_query($conn, "SELECT DISTINCT PanchayatId, PanchayatName, PhcId FROM hscmaster WHERE 
+		BlockId = '$BlockId' AND
+		PhcId = '$PhcId' AND
+		HscId = '$hscId' ORDER BY PanchayatId;");
         $resultKey = "PanchayatId";
         $resultVal = "PanchayatName";
         ?>
             <option value="">All Panchayat</option>
     <?php } else {
          $panchayatId = $_POST["panchayatId"];
-        $result = mysqli_query($conn, "SELECT DISTINCT VillageId, VillageName, PhcId FROM hscmaster WHERE panchayatId = '$panchayatId' ORDER BY VillageId;");
+		$BlockId = $_POST["BlockId"];
+		$PhcId = $_POST["PhcId"];
+		$HscId = $_POST["HscId"];
+        $result = mysqli_query($conn, "SELECT DISTINCT VillageId, VillageName, PhcId FROM hscmaster 
+		WHERE 
+		BlockId = '$BlockId' AND
+		PhcId = '$PhcId' AND
+		HscId = '$HscId' AND
+		panchayatId = '$panchayatId' ORDER BY VillageId;");
          $resultKey = "VillageId";
         $resultVal = "VillageName";
         ?>
