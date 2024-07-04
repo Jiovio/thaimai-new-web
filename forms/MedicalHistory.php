f03612fc213edb55c41967da6388a318cbb3f75f
nithyajiovio <nithya.jiovio@outlook.com>
2024-07-04 18:18:36 +0530

    fixing c panel issues

--- forms/MedicalHistory.php
+++ forms/MedicalHistory.php
@@ -41,7 +41,7 @@
                          </tr>
                        </thead>
 <?php
-  $listQry = "SELECT mh.picmeno,ec.motheraadhaarid,mh.id,mh.lmpdate,mh.edddate,mh.reg12weeks,ec.motheraadhaarname,mh.createdBy,ec.BlockId,ec.PhcId,ec.HscId FROM medicalhistory mh JOIN ecregister ec on ec.picmeNo=mh.picmeno WHERE mh.status=1";
+  $listQry = "SELECT mh.picmeno,ec.motheraadhaarid,mh.id,mh.lmpdate,mh.edddate,mh.reg12weeks,ec.motheraadhaarname,mh.createdBy,ec.BlockId,ec.PhcId,ec.HscId FROM medicalhistory mh WHERE mh.status=1";
   $private = "";
   $orderQry = "";
  //  $listQry = "SELECT DISTINCT(mh.picmeno),ec.motheraadhaarid,mh.id,mh.lmpdate,mh.edddate,mh.reg12weeks,ec.motheraadhaarname,mh.createdBy,ec.BlockId,ec.PhcId,ec.HscId FROM medicalhistory mh JOIN ecregister ec on ec.picmeNo=mh.picmeno WHERE mh.status=1";
