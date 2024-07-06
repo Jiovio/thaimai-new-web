454ecd6091a8e929828a22221cc6e85bb5cde17e
nithyajiovio <nithya.jiovio@outlook.com>
2024-07-06 14:07:17 +0530

    fixed c panel count issue for dashboard

--- forms/LoadBlock.php
+++ forms/LoadBlock.php
@@ -10,7 +10,7 @@ $ArCnt = mysqli_fetch_array($ArCntmq);
 
 //$AvCntmq = mysqli_query($conn,"SELECT COUNT(DISTINCT(av.picmeno)) AS AvCnt FROM antenatalvisit av JOIN ecregister ec ON av.picmeno=ec.picmeno WHERE ec.BlockId='".$bloName."' AND av.status=1");
 //$AvCntmq = mysqli_query($conn,"SELECT SUM(1) AS AvCnt FROM antenatalvisit av where ec.BlockId='".$bloName."' AND av.status=1 AND ancPeriod = 1 GROUP BY status");
-$AvCntmq = mysqli_query($conn,"SELECT COUNT((av.picmeno)) AS AvCnt FROM antenatalvisit av JOIN ecregister ec ON av.picmeno=ec.picmeno WHERE ec.BlockId='".$bloName."' AND av.status=1");
+$AvCntmq = mysqli_query($conn,"SELECT COUNT(av.picmeno) AS AvCnt FROM antenatalvisit av JOIN ecregister ec ON av.picmeno=ec.picmeno WHERE ec.BlockId='".$bloName."' AND av.status=1");
 $AvCnt = mysqli_fetch_array($AvCntmq);
 //$AvCnt = 0;
 
