20ed718a91f6bfbb113bd918ba85230aa46317c0
nithyajiovio <nithya.jiovio@outlook.com>
2024-09-30 19:23:34 +0530

    fixing api issue

--- api/ecregister_api/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\EcregisterController;
-
-Route::get('ecregisters', [EcregisterController::class, 'index']);
-Route::get('ecregisters/{id}', [EcregisterController::class, 'show']);
-Route::post('/ecregisters', [EcregisterController::class, 'store']);
-Route::put('ecregisters/{id}', [EcregisterController::class, 'update']);
-Route::delete('ecregisters/{id}', [EcregisterController::class, 'destroy']);
